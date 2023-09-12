7d22ab7e076bb8fb07790d3ed41fe772c25ddae5
Riccardo Fano <rcr.fano@gmail.com>
2023-09-12 10:22:49 +0200

    Solve part one of day one

---
+++ src/day/01.test.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "bun:test";
+import { readExample } from "@advent-of-code";
+import { partOne, partTwo } from "./01";
+
+test("Day 01 - Part one", () => {
+    const input = readExample(1);
+    expect(partOne(input)).toEqual(7);
+});
+
+test("Day 01 - Part two", () => {
+    const input = readExample(1);
+    expect(partTwo(input)).toEqual(null);
+});
---
+++ src/day/01.ts
@@ -0,0 +1,28 @@
+export function partOne(input: string): number | null {
+    const lines = input.trim().split("\n");
+    let increases = 0;
+
+    for (let i = 1; i < lines.length; i++) {
+        if (+lines[i] > +lines[i - 1]) {
+            increases++;
+        }
+    }
+
+    return increases;
+}
+
+export function partTwo(input: string): number | null {
+    return null;
+}
+
+import { solve, readInput } from "@advent-of-code";
+
+function main() {
+    const input = readInput(1);
+    solve(partOne, input);
+    solve(partTwo, input);
+}
+
+if (process.env.NODE_ENV !== "test") {
+    main();
+}
