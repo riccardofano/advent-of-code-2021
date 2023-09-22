69c9c5ea9c2222f85b0bc39e448e7d24c6607905
Riccardo Fano <rcr.fano@gmail.com>
2023-09-22 10:49:10 +0200

    Solve second part of day five

--- src/day/05.ts
+++ src/day/05.ts
@@ -11,24 +11,42 @@ function parseLine(line: string): Line {
     const [x1, y1] = range1.split(",").map(Number);
     const [x2, y2] = range2.split(",").map(Number);
 
-    const xRange: Range = x1 < x2 ? [x1, x2] : [x2, x1];
-    const yRange: Range = y1 < y2 ? [y1, y2] : [y2, y1];
-
-    return [xRange, yRange];
+    return [
+        [x1, x2],
+        [y1, y2],
+    ];
 }
 
-function addPoints([x, y]: Line, map: Map<string, number>) {
+function addPoints(
+    [x, y]: Line,
+    map: Map<string, number>,
+    calcDiagonal: boolean
+) {
+    const normX: Range = x[0] < x[1] ? [x[0], x[1]] : [x[1], x[0]];
+    const normY: Range = y[0] < y[1] ? [y[0], y[1]] : [y[1], y[0]];
     const isVertical = x[0] === x[1];
     const isHorizontal = y[0] === y[1];
 
     if (isHorizontal) {
-        for (let i = x[0]; i <= x[1]; i++) {
+        for (let i = normX[0]; i <= normX[1]; i++) {
             const point = pointToString([i, y[0]]);
             map.set(point, (map.get(point) || 0) + 1);
         }
     } else if (isVertical) {
-        for (let i = y[0]; i <= y[1]; i++) {
+        for (let i = normY[0]; i <= normY[1]; i++) {
             const point = pointToString([x[0], i]);
+            map.set(point, (map.get(point) || 0) + 1);
+        }
+    } else if (calcDiagonal) {
+        const range = Math.abs(x[1] - x[0]);
+        const xDelta = x[0] < x[1] ? 1 : -1;
+        const yDelta = y[0] < y[1] ? 1 : -1;
+
+        for (let i = 0; i <= range; i++) {
+            const newX = x[0] + i * xDelta;
+            const newY = y[0] + i * yDelta;
+
+            const point = pointToString([newX, newY]);
             map.set(point, (map.get(point) || 0) + 1);
         }
     }
@@ -39,7 +57,7 @@ export function partOne(input: string): number | null {
 
     for (const row of input.trim().split("\n")) {
         const line = parseLine(row);
-        addPoints(line, map);
+        addPoints(line, map, false);
     }
 
     let highDangerPoints = 0;
@@ -53,7 +71,21 @@ export function partOne(input: string): number | null {
 }
 
 export function partTwo(input: string): number | null {
-    return null;
+    const map = new Map<string, number>();
+
+    for (const row of input.trim().split("\n")) {
+        const line = parseLine(row);
+        addPoints(line, map, true);
+    }
+
+    let highDangerPoints = 0;
+    for (const intersection of map.values()) {
+        if (intersection > 1) {
+            highDangerPoints++;
+        }
+    }
+
+    return highDangerPoints;
 }
 
 import { solve, readInput } from "@advent-of-code";
